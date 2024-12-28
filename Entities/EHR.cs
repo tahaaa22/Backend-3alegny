d085c3f4214e3c4d242f52aa9ca38ff18d9cda29
ahmed mohamed taha <128100857+tahaaa22@users.noreply.github.com>
2024-12-28 02:44:36 +0200

    Update EHR.cs

--- Entities/EHR.cs
+++ Entities/EHR.cs
@@ -1,27 +1,30 @@
-﻿using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace _3alegny.Entities
 {
-    public class PHR
+    public class EHR
     {
-        [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
-        public ObjectId PHRId { get; set; }
-        public string PatientId { get; set; }
-        public string Notes { get; set; }
+        public ObjectId EMR_id { get; set; }
+        public int PatientId { get; set; }
+        public string Description { get; set; }
         public string AdminId { get; set; }
         public Admin Admin { get; set; }
-        public Patient Patient { get; set; }
-        public string? MedicalCondition { get; set; }
-        public string Allergies { get; set; }
-        public string ChronicIllness { get; set; }
-        public string Diagnosis { get; set; }
-        public string Medication { get; set; }
-        public string FamilyHistory { get; set; }
-        public string ImagingResults { get; set; }
-        public string LabResults { get; set; }
-        public string MedicalProcedures { get; set; }
-        public string PrescriptionHistory { get; set; }
+        public string HospitalId { get; set; }
+        public Hospital Hospital { get; set; }
+        public string? Diagnosis { get; set; }
+        public Doctors Doctor { get; set; }
+        public string? Prescription { get; set; }
+        public string? Notes { get; set; }
+        [JsonProperty("LabResults")]
+        public List<string> LabResults { get; set; } = [];
+        public string? ImagingResults { get; set; }
+        public string? ProcedureHistory { get; set; }
+        public string? MedicationHistory { get; set; }
+        public string? Followup { get; set; } = null;
+
     }
+    
 }
